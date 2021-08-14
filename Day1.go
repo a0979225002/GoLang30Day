7ed51a7986826332ecc430b98f3e95f32934f3bd
XIAO_LI_PIN_MAC <you@example.com>
2021-08-14 23:07:16 +0800

    Initial commit

---
+++ Day1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var x,y,z int
+
+var  c,typescript,java string
+
+func main()  {
+
+	c = "C"
+	typescript = "TypeScript"
+	java = "JAVA"
+	fmt.Println(x,y,z,c,typescript,java)
+}
